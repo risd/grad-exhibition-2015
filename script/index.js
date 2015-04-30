9b85ba240e8d5837a13587e96b7f1061ef573cf8
rubillionaire <mail@rubenrodriguez.me>
2015-04-30 13:42:32 -0700

    Styles starting
    pulled in SVGs as well.

--- script/index.js
+++ script/index.js
@@ -1,1 +1,68 @@
+var Poster = require('./poster.js')('.poster');
+var Info = require('./info.js')('.info');
 
+var Router = require('routes');
+var router = Router();
+
+router.addRoute('/', function () {
+    console.log('route: /');
+
+});
+
+router.addRoute('/info', function () {
+    console.log('route: /info');
+});
+
+
+(function initialize (href) {
+
+    routeClicks();
+
+    var route = router.match(href);
+    route.fn.apply(window, [route]);
+
+    window.ga = window.ga ||
+                function() {
+                    (ga.q=ga.q||[])
+                        .push(arguments);
+                };
+
+    ga.l = +new Date;
+    ga('create', 'UA-59960857-1', 'auto');
+    ga('set', 'forceSSL', true);
+    ga('set', 'page', href);
+    ga('send', 'pageview');
+
+})(window.location.pathname);
+
+
+function routeClicks () {
+    var base = window.location.host;
+
+    var links = document.getElementsByTagName('a');
+
+    for (var i = links.length - 1; i >= 0; i--) {
+        if (links[i].classList.contains('external-link')) {
+            // console.log('Not internally handled');
+        } else {
+            links[i].onclick = interllayHandleClick;
+        }
+    }
+
+    function interllayHandleClick (event) {
+        event.preventDefault();
+        var target = findAnchor(event.target);
+        var href = target.href.split(base)[1];
+        window.history.pushState('', '', href);
+
+        var route = router.match(href);
+        route.fn.apply(window, [route]);
+
+        if (ga) {
+            ga('set', 'page', href);
+            ga('send', 'pageview');
+        }
+
+        return false;
+    }
+}
--- static/script.js
+++ static/script.js
@@ -1,4 +1,241 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.routes=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+
+var localRoutes = [];
 
 
-},{}]},{},[1]);
+/**
+ * Convert path to route object
+ *
+ * A string or RegExp should be passed,
+ * will return { re, src, keys} obj
+ *
+ * @param  {String / RegExp} path
+ * @return {Object}
+ */
+
+var Route = function(path){
+  //using 'new' is optional
+
+  var src, re, keys = [];
+
+  if(path instanceof RegExp){
+    re = path;
+    src = path.toString();
+  }else{
+    re = pathToRegExp(path, keys);
+    src = path;
+  }
+
+  return {
+  	 re: re,
+  	 src: path.toString(),
+  	 keys: keys
+  }
+};
+
+/**
+ * Normalize the given path string,
+ * returning a regular expression.
+ *
+ * An empty array should be passed,
+ * which will contain the placeholder
+ * key names. For example "/user/:id" will
+ * then contain ["id"].
+ *
+ * @param  {String} path
+ * @param  {Array} keys
+ * @return {RegExp}
+ */
+var pathToRegExp = function (path, keys) {
+	path = path
+		.concat('/?')
+		.replace(/\/\(/g, '(?:/')
+		.replace(/(\/)?(\.)?:(\w+)(?:(\(.*?\)))?(\?)?|\*/g, function(_, slash, format, key, capture, optional){
+			if (_ === "*"){
+				keys.push(undefined);
+				return _;
+			}
+
+			keys.push(key);
+			slash = slash || '';
+			return ''
+				+ (optional ? '' : slash)
+				+ '(?:'
+				+ (optional ? slash : '')
+				+ (format || '') + (capture || '([^/]+?)') + ')'
+				+ (optional || '');
+		})
+		.replace(/([\/.])/g, '\\$1')
+		.replace(/\*/g, '(.*)');
+	return new RegExp('^' + path + '$', 'i');
+};
+
+/**
+ * Attempt to match the given request to
+ * one of the routes. When successful
+ * a  {fn, params, splats} obj is returned
+ *
+ * @param  {Array} routes
+ * @param  {String} uri
+ * @return {Object}
+ */
+var match = function (routes, uri, startAt) {
+	var captures, i = startAt || 0;
+
+	for (var len = routes.length; i < len; ++i) {
+		var route = routes[i],
+		    re = route.re,
+		    keys = route.keys,
+		    splats = [],
+		    params = {};
+
+		if (captures = uri.match(re)) {
+			for (var j = 1, len = captures.length; j < len; ++j) {
+				var key = keys[j-1],
+					val = typeof captures[j] === 'string'
+						? unescape(captures[j])
+						: captures[j];
+				if (key) {
+					params[key] = val;
+				} else {
+					splats.push(val);
+				}
+			}
+			return {
+				params: params,
+				splats: splats,
+				route: route.src,
+				next: i + 1
+			};
+		}
+	}
+};
+
+/**
+ * Default "normal" router constructor.
+ * accepts path, fn tuples via addRoute
+ * returns {fn, params, splats, route}
+ *  via match
+ *
+ * @return {Object}
+ */
+
+var Router = function(){
+  //using 'new' is optional
+  return {
+    routes: [],
+    routeMap : {},
+    addRoute: function(path, fn){
+      if (!path) throw new Error(' route requires a path');
+      if (!fn) throw new Error(' route ' + path.toString() + ' requires a callback');
+
+      if (this.routeMap[path]) {
+        throw new Error('path is already defined: ' + path);
+      }
+
+      var route = Route(path);
+      route.fn = fn;
+
+      this.routes.push(route);
+      this.routeMap[path] = fn;
+    },
+
+    match: function(pathname, startAt){
+      var route = match(this.routes, pathname, startAt);
+      if(route){
+        route.fn = this.routeMap[route.route];
+        route.next = this.match.bind(this, pathname, route.next)
+      }
+      return route;
+    }
+  }
+};
+
+Router.Route = Route
+Router.pathToRegExp = pathToRegExp
+Router.match = match
+// back compat
+Router.Router = Router
+
+module.exports = Router
+
+},{}]},{},[1])
+(1)
+});
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],2:[function(require,module,exports){
+var Poster = require('./poster.js')('.poster');
+var Info = require('./info.js')('.info');
+
+var Router = require('routes');
+var router = Router();
+
+router.addRoute('/', function () {
+    console.log('route: /');
+
+});
+
+router.addRoute('/info', function () {
+    console.log('route: /info');
+});
+
+
+(function initialize (href) {
+
+    routeClicks();
+
+    var route = router.match(href);
+    route.fn.apply(window, [route]);
+
+    window.ga = window.ga ||
+                function() {
+                    (ga.q=ga.q||[])
+                        .push(arguments);
+                };
+
+    ga.l = +new Date;
+    ga('create', 'UA-59960857-1', 'auto');
+    ga('set', 'forceSSL', true);
+    ga('set', 'page', href);
+    ga('send', 'pageview');
+
+})(window.location.pathname);
+
+
+function routeClicks () {
+    var base = window.location.host;
+
+    var links = document.getElementsByTagName('a');
+
+    for (var i = links.length - 1; i >= 0; i--) {
+        if (links[i].classList.contains('external-link')) {
+            // console.log('Not internally handled');
+        } else {
+            links[i].onclick = interllayHandleClick;
+        }
+    }
+
+    function interllayHandleClick (event) {
+        event.preventDefault();
+        var target = findAnchor(event.target);
+        var href = target.href.split(base)[1];
+        window.history.pushState('', '', href);
+
+        var route = router.match(href);
+        route.fn.apply(window, [route]);
+
+        if (ga) {
+            ga('set', 'page', href);
+            ga('send', 'pageview');
+        }
+
+        return false;
+    }
+}
+},{"./info.js":3,"./poster.js":4,"routes":1}],3:[function(require,module,exports){
+
+},{}],4:[function(require,module,exports){
+arguments[4][3][0].apply(exports,arguments)
+},{"dup":3}]},{},[2]);
