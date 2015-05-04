c20f2fae2efe2504dd7fbe2d82b61e6f193b6688
rubillionaire <mail@rubenrodriguez.me>
2015-05-04 18:18:48 -0400

    first

--- script/statement.js
+++ script/statement.js
@@ -6,26 +6,44 @@ function Statement (selector) {
     if (!(this instanceof Statement)) return new Statement(selector);
     if (!selector) throw new Error('Requires selector.');
 
+    this.name = 'statement';
+    this.active = false;
     this.selector = selector;
     this.background = document.querySelector(selector + '-background');
     this.foreground = document.querySelector(selector + '-foreground');
-    this.anchor = this.background.querySelector('a');
 }
 
-Statement.prototype.inActive = function () {
+Statement.prototype.setInActive = function () {
     var self = this;
-    this.anchor.href = '/statement';
+    this.active = false;
     this.background.classList.add('inActive');
     this.foreground.classList.add('inActive');
     this.background.classList.remove('active');
     this.foreground.classList.remove('active');
 };
 
-Statement.prototype.active = function () {
+Statement.prototype.setActive = function () {
     var self = this;
-    this.anchor.href = '/';
+    this.active = true;
     this.background.classList.add('active');
     this.foreground.classList.add('active');
     this.background.classList.remove('inActive');
     this.foreground.classList.remove('inActive');
+};
+
+Statement.prototype.clicked = function () {
+    var self = this;
+    var eventStream = through.obj();
+
+    this.foreground
+        .addEventListener('click', toggle, false);
+
+    function toggle (ev) {
+        eventStream.push({
+            name: self.name,
+            active: self.active
+        });
+    }
+
+    return eventStream;
 };
